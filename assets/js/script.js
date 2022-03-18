e61364d2b511615519178d4657e4007895b29a39
Ali Khalife <alikhalife05@gmail.com>
2022-03-18 12:26:22 +0100

    moving my spaceship left and right. It's still going out of the frame though

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-// creating canvas
+// CREATING CANVAS
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext("2d");
 
@@ -8,29 +8,31 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 
+
+//SPACESHIP
 class Player {
     constructor() {
 
 
         this.velocity = {
             x: 0,
-            // y: 0 - for this game I want my player to move from left to right
-            // so I don't really need it to move up and down. y is not necessary
+            y: 0,
+
         }
 
         const image = new Image();
         image.src = "./assets/img/spaceship.png"
 
-image.onload = () => {
-        const scale = 0.15;
-        this.image = image;
-        this.width = image.width * scale;
-        this.height = image.height * scale;
-        this.positiion = {
-            x: canvas.width / 2 - this.width / 2,
-            y: canvas.height - this.height - 50
+        image.onload = () => {
+            const scale = 0.15;
+            this.image = image;
+            this.width = image.width * scale;
+            this.height = image.height * scale;
+            this.positiion = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height - this.height - 50
+            }
         }
-    }
 
     }
 
@@ -39,7 +41,7 @@ image.onload = () => {
         // once it is verified, I no longer need it, so I commented it out
         // context.fillStyle = "red";
         // context.fillRect(this.positiion.x, this.positiion.y, this.width, this.height)
-       if(this.image)
+
         context.drawImage(
             this.image,
             this.positiion.x,
@@ -48,12 +50,37 @@ image.onload = () => {
             this.height
         )
     }
+
+    update() {
+        if (this.image) {
+            this.draw()
+            this.positiion.x += this.velocity.x;
+        }
+
+
+    }
 }
 
+
+//VARIABLES
 const player = new Player();
-player.draw();
+const keys = {
+    ArrowLeft: {
+        pressed: false
+    },
+
+    ArrowRight: {
+        pressed: false
+    },
+
+    space: {
+        pressed: false
+    }
+}
 
 
+
+// ANIMATION
 function animate() {
     requestAnimationFrame(animate); // looping on image so that it appears on browser
 
@@ -61,7 +88,64 @@ function animate() {
     context.fillStyle = "black"
     context.fillRect(0, 0, canvas.width, canvas.height)
 
-    player.draw(); // calling the image
+    player.update(); // calling the image
+
+    if (keys.ArrowLeft.pressed) {
+        player.velocity.x = -5;
+    }
+    else if (keys.ArrowRight.pressed) {
+        player.velocity.x = 5;
+    }
+    else {
+        player.velocity.x = 0;
+    }
+
+
 }
 
 animate();
+
+
+
+//moving player
+addEventListener("keydown", ({ key }) => {
+    switch (key) {
+        case "ArrowLeft":
+            console.log("left")
+            // player.velocity.x += -5
+            keys.ArrowLeft.pressed = true;
+            break
+
+        case "ArrowRight":
+            console.log("right")
+            // player.velocity.x += 5
+            keys.ArrowRight.pressed = true;
+            break
+
+        case " ":
+            console.log("space")
+            break
+    }
+
+})
+
+
+
+addEventListener("keyup", ({ key }) => {
+    switch (key) {
+        case "ArrowLeft":
+            console.log("left")
+            keys.ArrowRight.pressed = false;
+            break
+
+        case "ArrowRight":
+            console.log("right")
+            keys.ArrowLeft.pressed = false;
+            break
+
+        case " ":
+            console.log("space")
+            break
+    }
+
+})
